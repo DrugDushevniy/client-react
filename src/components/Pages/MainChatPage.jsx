b173c1f7e85cc09662c5c41ed42f474616ae3f2f
strupsts <strupsts3@mail.ru>
2023-02-25 16:34:12 +0500

    AUTH LOGICK CLIENT

--- src/components/Pages/MainChatPage.jsx
+++ src/components/Pages/MainChatPage.jsx
@@ -20,7 +20,7 @@ const MainChatPage = () => {
 
     useEffect(()=>{
         // ws://87.249.44.57:5000/ws
-        socket = new WebSocket("ws://localhost:5000/ws")
+        socket = new WebSocket("ws://87.249.44.57:5000/ws")
         socket.onopen = (event) => {
             console.log('Client: Ты подключился');
             socket.send(JSON.stringify({
